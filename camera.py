0a0c4ff658a77f6e01673b2d2cd9cfd9173ae4de
Piyush Malhotra <piyushmalhotra28@gmail.com>
2017-09-08 11:28:41 +0530

    Create opencv videocapture module

---
+++ camera.py
@@ -0,0 +1,50 @@
+import numpy as np
+import cv2
+from filter import apply_filter
+from model import FaceKeypointsCaptureModel
+
+rgb = cv2.VideoCapture(0)
+facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+labels = {}
+
+def __get_data__():
+    """
+    __get_data__: Gets data from the VideoCapture object and classifies them
+    to a face or no face. 
+    
+    returns: tuple (faces in image, frame read)
+    """
+    _, fr = rgb.read()
+    gray = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+    faces = facec.detectMultiScale(gray, 1.3, 5)
+    
+    return faces, fr, gray
+
+def start_app(cnn):
+    skip_frame = 10
+    data = []
+    flag = False
+    ix = 0
+    while True:
+        ix += 1
+        
+        faces, fr, gray_fr = __get_data__()
+        for (x, y, w, h) in faces:
+            fc = gray_fr[y:y+h, x:x+w]
+            
+            roi = cv2.resize(fc, (96, 96))
+            pred, pred_dict = cnn.predict_points(roi[np.newaxis, :, :, np.newaxis])
+            pred, pred_dict = cnn.scale_prediction((x, fc.shape[1]+x), (y, fc.shape[0]+y))
+
+            fr = apply_filter(fr, pred_dict)
+            #cv2.rectangle(fr, (x, y), (x+w, y+h), (255, 0, 0))
+            #cv2.putText(fr, labels[int(pred)], (x, y), cv2.FONT_HERSHEY_COMPLEX, 2, 255)
+        if cv2.waitKey(1) == 27:
+            break
+        cv2.imshow('rgb', fr)
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    model = FaceKeypointsCaptureModel("face_model.json", "face_model.h5")
+    start_app(model)
